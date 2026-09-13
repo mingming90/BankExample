69c594faf071b698eb846e390b6f40d64a0abba6
Dinh Ngoc Tao <DinhNgoc.Tao@parasoftsea.onmicrosoft.com>
2026-09-13 21:58:20 +0700

    Initial commit

---
+++ Parasoft.Dottest.Examples.Bank.Tests.NUnit/BankUserTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parasoft.Dottest.Examples.Bank.Tests.NUnit
+{
+    [TestFixture]
+    public class BankUserTests
+    {
+        private static IEnumerable<string> Mails = new List<string> { "foo.bar", "", null, "@one.com", "foo.bar@foo.com.com" };
+
+        [Test]
+        [TestCaseSource("Mails")]
+        public void Test(string sourceMail)
+        {
+            var name = "John";
+            var sirName = "White";
+            var mail = sourceMail;
+            var pass = "$tr0ngp4$$;)";
+            var user = new BankUser(name, sirName, mail, pass);
+
+            Assert.That(user.Name == name);
+            Assert.That(user.SirName == sirName);
+            Assert.That(user.Login == mail);
+            Assert.That(user.Password == pass);
+
+            Assert.That(!user.CheckEmail());
+        }
+    }
+}
